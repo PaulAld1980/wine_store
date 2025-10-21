98c79788542ca8a9e308e958b712fff70661c711
Paul <hidden80@mail.ru>
2025-10-22 01:15:55 +0500

    Add dynamic winery age

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+from datetime import datetime
+from jinja2 import Environment, FileSystemLoader
 
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def main():
+    current_year = datetime.now().year
+
+    foundation_year = 1920
+    age = current_year - foundation_year
+
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template('template.html')
+
+    rendered_page = template.render(age=age)
+
+    with open('index.html', 'w', encoding='utf-8') as file:
+        file.write(rendered_page)
+
+    print('✅ Страница index.html успешно сгенерирована!')
+
+
+if __name__ == '__main__':
+    main()
