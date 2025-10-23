b8fe8cc2f599397cad9feec2e5076acb6211ec4b
Paul <hidden80@mail.ru>
2025-10-24 03:19:24 +0500

    feat(main): Add Excel data reading and JSON output

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from jinja2 import Environment, FileSystemLoader
+import pandas as pd
 
 
 def get_year_word(age):
@@ -20,10 +21,22 @@ def main():
     foundation_year = 1920
     age = current_year - foundation_year
 
+    excel_data_df = pd.read_excel('wine.xlsx', na_values='', keep_default_na=False)
+    json_data = excel_data_df.to_json(orient='records', force_ascii=False)
+
+    with open('wine.json', 'w', encoding='utf-8') as f:
+        f.write(json_data)
+
+    print(json_data)
+
     env = Environment(loader=FileSystemLoader('.'))
     template = env.get_template('template.html')
 
-    rendered_page = template.render(age=age, get_year_word=get_year_word)
+    rendered_page = template.render(
+        age=age,
+        get_year_word=get_year_word,
+        wines=excel_data_df.to_dict(orient='records')
+    )
 
     with open('index.html', 'w', encoding='utf-8') as file:
         file.write(rendered_page)
